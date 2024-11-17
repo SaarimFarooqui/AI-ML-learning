b04c39857f89f55e4ff7886f9f1b1bba86923d62
Saarim Farooqui <saarimfarooqui000@gmail.com>
2024-11-17 19:44:02 +0500

    Histogram

---
+++ Histogram.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as mp
+
+x = np.random.uniform(0,5,130)
+
+mp.hist(x,5)
+mp.show()
+
--- RandomFloats.py
+++ RandomFloats.py
@@ -1,3 +1,2 @@
 import numpy as np
-
 x = np.random.uniform(1,100,25)
