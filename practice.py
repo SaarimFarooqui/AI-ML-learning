dffbb3334adacb8755231c586742d09c84045010
Saarim Farooqui <saarimfarooqui000@gmail.com>
2024-11-25 11:47:40 +0500

    Histogram Practice

--- practice.py
+++ practice.py
@@ -1,8 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-ages = [9,9,9,9,11,12,13,14,15,11,12,13,14,10,9,11,12,13,14,12,11,13,14]
-percentile = 50
-value = np.percentile(ages, 50)
-print(f"The { percentile }% of data falls under the value: ",value)
-
+numbers = np.random.uniform(1,10,100)
+plt.hist(numbers)
+plt.show()
